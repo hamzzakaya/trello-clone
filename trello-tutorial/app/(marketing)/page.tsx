f727728e314685236ac44a7536dd67c3d940481f
HAMZAKAYA <hamzakaya4343@gmail.com>
2024-03-12 05:03:50 +0300

    adding a button with shadcn-ui

--- trello-tutorial/app/(marketing)/page.tsx
+++ trello-tutorial/app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
 
 const MarketingPage = () => {
@@ -19,6 +20,9 @@ const MarketingPage = () => {
       <div className=" text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
         Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
       </div>
+      <Button>
+        
+      </Button>
     </div>
   );
 };
