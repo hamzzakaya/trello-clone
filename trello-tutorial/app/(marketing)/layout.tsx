afa3979e97d53865e30f9147bac2e61fb81040ca
HAMZAKAYA <hamzakaya4343@gmail.com>
2024-03-14 05:02:22 +0300

    create navbar

--- trello-tutorial/app/(marketing)/layout.tsx
+++ trello-tutorial/app/(marketing)/layout.tsx
@@ -1,3 +1,5 @@
+import { Navbar } from "./_components/navbar";
+
 const MarketingLayout = ({
     children
 }:{
@@ -5,12 +7,12 @@ const MarketingLayout = ({
 }) => {
     return(
         <div className="h-full bg-slate-100">
-       
+            <Navbar />
         <main className="pt-40 pb-20 bg-slate-100">
             {children}
         </main>
         </div>
-    )
-}
+    );
+};
 
-export default MarketingLayout
+export default MarketingLayout;
